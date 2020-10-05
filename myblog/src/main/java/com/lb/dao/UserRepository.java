cc6cd5e4aef30bfbf1b48c872a004663fd29fd8f
FYF <836890287@qq.com>
2020-10-05 23:37:27 +0800

    更换文件

--- myblog/src/main/java/com/lb/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lb.dao;
-
-import com.lb.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsernameAndPassword(String username,String password);
-}
